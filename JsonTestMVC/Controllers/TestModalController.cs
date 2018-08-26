0aebdaf5641c7baf33648d114e3d9f058a565329
tridip-bba <tridip@bba-reman.com>
2018-08-27 00:54:21 +0530

    bootstrap modal implemented

---
+++ JsonTestMVC/Controllers/TestModalController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using JsonTestMVC.Models;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using JsonTestMVC.Utilities;
+
+namespace JsonTestMVC.Controllers
+{
+    public class TestModalController : Controller
+    {
+        // GET: TestModal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowBootStrapModal(int testid1, int testid2)
+        {
+            ViewBag.testid1 = testid1;
+            ViewBag.testid2 = testid2;
+            return PartialView("_TestPartial");
+        }
+
+        public ActionResult GeneratePDF()
+        {
+            bool IsPdfGenerated = false;
+
+            List<Student> studentsVM = new List<Student>
+            {
+                new Student {ID=1,FirstName="Joy",      LastName="Roy",     FavouriteGames="Hocky"},
+                new Student {ID=2,FirstName="Raja",     LastName="Basu",    FavouriteGames="Cricket"},
+                new Student {ID=3,FirstName="Arijit",   LastName="Banerjee",FavouriteGames="Foot Ball"},
+                new Student {ID=4,FirstName="Dibyendu", LastName="Saha",    FavouriteGames="Tennis"},
+                new Student {ID=5,FirstName="Sanjeeb",  LastName="Das",     FavouriteGames="Hocky"},
+            };
+
+            var viewToString = StringUtilities.RenderViewToString(ControllerContext, "~/Views/Shared/_Report.cshtml", studentsVM, true);
+            string filepath = HttpContext.Server.MapPath("~/PDFArchives/") + "mypdf.pdf";
+
+            try
+            {
+                StringReader sr = new StringReader(viewToString);
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 30f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, new FileStream(filepath, FileMode.Create));
+                pdfDoc.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                pdfDoc.Close();
+                IsPdfGenerated = true;
+            }
+            catch (Exception ex)
+            {
+                IsPdfGenerated = false;
+            }
+
+            //return Json(new { fileName = filepath, errorMessage = (IsPdfGenerated ? "" : "Error occured when pdf generated") });
+            return Json(new { Msg = (IsPdfGenerated ? "PDF generated properly" : "Error occured when pdf generated") });
+        }
+    }
+}
---
+++ JsonTestMVC/Utilities/Utlities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace JsonTestMVC.Utilities
+{
+    public static class StringUtilities
+    {
+        public static string RenderViewToString(System.Web.Mvc.ControllerContext context, string viewPath, object model = null, bool partial = false)
+        {
+            // first find the ViewEngine for this view
+            ViewEngineResult viewEngineResult = null;
+            if (partial)
+            {
+                viewEngineResult = ViewEngines.Engines.FindPartialView(context, viewPath);
+            }
+            else
+            {
+                viewEngineResult = ViewEngines.Engines.FindView(context, viewPath, null);
+            }
+
+            if (viewEngineResult == null)
+            {
+                throw new FileNotFoundException("View cannot be found.");
+            }
+
+            // get the view and attach the model to view data
+            var view = viewEngineResult.View;
+            context.Controller.ViewData.Model = model;
+
+            string result = null;
+
+            using (var sw = new StringWriter())
+            {
+                var ctx = new ViewContext(context, view, context.Controller.ViewData, context.Controller.TempData, sw);
+                view.Render(ctx, sw);
+                result = sw.ToString();
+            }
+
+            return result.Trim();
+        }
+    }
+}
